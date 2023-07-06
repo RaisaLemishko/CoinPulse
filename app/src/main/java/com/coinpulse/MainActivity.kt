d71039c919c77ae08f1177006194a2a2e58db749
Raisa Lemishko <raisa.lemishko@gmail.com>
2023-07-06 17:10:28 +0300

    Initial commit

---
+++ app/src/main/java/com/coinpulse/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.coinpulse
+
+import CoinPulseTheme
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CoinPulseTheme {
+                Surface(color = MaterialTheme.colorScheme.background) {
+
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/coinpulse/ui/theme/Theme.kt
@@ -0,0 +1,52 @@
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import com.coinpulse.ui.theme.ColorPrimary
+import com.coinpulse.ui.theme.DarkGray
+import com.coinpulse.ui.theme.MediumGray
+import com.coinpulse.ui.theme.Shapes
+import com.coinpulse.ui.theme.TextWhite
+import com.coinpulse.ui.theme.Typography
+
+private val DarkColorPalette = darkColorScheme(
+    primary = ColorPrimary,
+    background = DarkGray,
+    onBackground = TextWhite,
+    onPrimary = DarkGray
+)
+
+private val LightColorPalette = lightColorScheme(
+    primary = ColorPrimary,
+    background = Color.White,
+    onBackground = MediumGray,
+    onPrimary = DarkGray
+)
+
+@Composable
+fun CoinPulseTheme(
+    useDarkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    // Dynamic color is available on Android 12+
+    val dynamicColor = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+    val colorScheme = when {
+        dynamicColor && useDarkTheme -> dynamicDarkColorScheme(LocalContext.current)
+        dynamicColor && !useDarkTheme -> dynamicLightColorScheme(LocalContext.current)
+        useDarkTheme -> DarkColorPalette
+        else -> LightColorPalette
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
