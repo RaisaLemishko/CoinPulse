d7ec54e36747d2e4e22d2227bf18fb815cbcf603
Raisa Lemishko <raisa.lemishko@gmail.com>
2023-07-07 12:13:49 +0300

    Set Up Dependency Injection

---
+++ app/src/main/java/com/coinpulse/di/AppModule.kt
@@ -0,0 +1,34 @@
+package com.coinpulse.di
+
+import com.coinpulse.common.Constants
+import com.coinpulse.data.data.CoinRepositoryImpl
+import com.coinpulse.data.remote.CoinPaprikaApi
+import com.coinpulse.domain.repository.CoinRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun providePaprikaApi(): CoinPaprikaApi {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CoinPaprikaApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCoinRepository(api: CoinPaprikaApi): CoinRepository {
+        return CoinRepositoryImpl(coinPaprikaApi = api)
+    }
+}
