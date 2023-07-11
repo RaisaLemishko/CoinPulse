629b29ec4e5fcc503586671ec6ead48def72d8e2
Raisa Lemishko <raisa.lemishko@gmail.com>
2023-07-11 18:00:09 +0300

    Implement Coin Details Screen

---
+++ app/src/main/java/com/coinpulse/presentation/coin_details/CoinDetailsViewModel.kt
@@ -0,0 +1,54 @@
+package com.coinpulse.presentation.coin_details
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.coinpulse.common.Constants.PARAM_COIN_ID
+import com.coinpulse.common.Resource
+import com.coinpulse.domain.use_case.get_coin_details.GetCoinDetailsUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import javax.inject.Inject
+
+@HiltViewModel
+class CoinDetailsViewModel
+@Inject constructor(
+    private val coinDetailsUseCase: GetCoinDetailsUseCase,
+    savedStateHandle: SavedStateHandle
+) : ViewModel() {
+
+    private val _state = mutableStateOf(CoinDetailsState())
+    val state: State<CoinDetailsState> = _state
+
+    init {
+        savedStateHandle.get<String>(PARAM_COIN_ID)?.let { coinId ->
+            getCoinDetails(coinId)
+        }
+    }
+
+    private fun getCoinDetails(coinId: String) {
+        coinDetailsUseCase(coinId).onEach { result ->
+            when (result) {
+                is Resource.Success -> {
+                    _state.value = CoinDetailsState(
+                        coinDetails = result.data
+                    )
+                }
+
+                is Resource.Error -> {
+                    _state.value = CoinDetailsState(
+                        error = result.message
+                            ?: "An unexpected error occurred"
+                    )
+                }
+
+                is Resource.Loading -> {
+                    _state.value = CoinDetailsState(isLoading = true)
+                }
+            }
+        }.launchIn(viewModelScope)
+    }
+}
---
+++ app/src/main/java/com/coinpulse/presentation/coin_details/components/CoinDetailsScreen.kt
@@ -0,0 +1,115 @@
+package com.coinpulse.presentation.coin_details.components
+
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.layout.Arrangement.SpaceBetween
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle.Companion.Italic
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.coinpulse.presentation.coin_details.CoinDetailsViewModel
+import com.google.accompanist.flowlayout.FlowRow
+
+@Composable
+fun CoinDetailsScreen(
+    viewModel: CoinDetailsViewModel = hiltViewModel()
+) {
+    val state = viewModel.state.value
+    Box(modifier = Modifier.fillMaxSize()) {
+        state.coinDetails?.let { coin ->
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                contentPadding = PaddingValues(20.dp)
+            ) {
+                item {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        horizontalArrangement = SpaceBetween
+                    ) {
+                        Text(
+                            text = "${coin.rank}. ${coin.name} (${coin.symbol})",
+                            style = MaterialTheme.typography.headlineMedium,
+                            modifier = Modifier.weight(8f)
+                        )
+                        Text(
+                            text = if (coin.isActive) "active" else "inactive",
+                            color = if (coin.isActive) Color.Green else Color.Red,
+                            fontStyle = Italic,
+                            textAlign = TextAlign.End,
+                            modifier = Modifier
+                                .align(CenterVertically)
+                                .weight(2f)
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text(
+                        text = coin.description,
+                        style = MaterialTheme.typography.bodySmall
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text(
+                        text = "Tags",
+                        style = MaterialTheme.typography.headlineSmall
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    FlowRow(
+                        mainAxisSpacing = 10.dp,
+                        crossAxisSpacing = 10.dp,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        coin.tags.forEach { tag ->
+                            CoinTag(tag = tag)
+                        }
+                    }
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text(
+                        text = "Team members",
+                        style = MaterialTheme.typography.headlineSmall
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                }
+                itemsIndexed(coin.team) { index, teamMember ->
+                    TeamListItem(
+                        teamMember = teamMember,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(10.dp)
+                    )
+                    Divider()
+                }
+            }
+        }
+        if (state.error.isNotBlank()) {
+            Text(
+                text = state.error,
+                color = MaterialTheme.colorScheme.error,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp)
+                    .align(Alignment.Center)
+            )
+        }
+        if (state.isLoading) {
+            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+        }
+    }
+}
