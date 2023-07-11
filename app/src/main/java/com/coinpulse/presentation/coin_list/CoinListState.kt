cddde22a210da4ba15d96275484aa877a26d7147
Raisa Lemishko <raisa.lemishko@gmail.com>
2023-07-11 17:59:03 +0300

    Implement Coin List Screen

---
+++ app/src/main/java/com/coinpulse/presentation/coin_list/CoinListState.kt
@@ -0,0 +1,9 @@
+package com.coinpulse.presentation.coin_list
+
+import com.coinpulse.domain.model.Coin
+
+data class CoinListState(
+    val isLoading: Boolean = false,
+    val coins: List<Coin> = emptyList(),
+    val error: String = ""
+)
---
+++ app/src/main/java/com/coinpulse/presentation/coin_list/CoinListViewModel.kt
@@ -0,0 +1,50 @@
+package com.coinpulse.presentation.coin_list
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.coinpulse.common.Resource
+import com.coinpulse.domain.use_case.get_coins.GetCoinsUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import javax.inject.Inject
+
+@HiltViewModel
+class CoinListViewModel
+@Inject constructor(
+    private val coinListUseCase: GetCoinsUseCase
+) : ViewModel() {
+
+    private val _state = mutableStateOf(CoinListState())
+    val state: State<CoinListState> = _state
+
+    init {
+        getCoins()
+    }
+
+    private fun getCoins() {
+        coinListUseCase().onEach { result ->
+            when (result) {
+                is Resource.Success -> {
+                    _state.value = CoinListState(
+                        isLoading = false, coins = result.data ?: emptyList()
+                    )
+                }
+
+                is Resource.Error -> {
+                    _state.value = CoinListState(
+                        isLoading = false,
+                        error = result.message
+                            ?: "An unexpected error occurred"
+                    )
+                }
+
+                is Resource.Loading -> {
+                    _state.value = CoinListState(isLoading = true)
+                }
+            }
+        }.launchIn(viewModelScope)
+    }
+}
